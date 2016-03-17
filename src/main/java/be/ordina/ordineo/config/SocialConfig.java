2559b3bfd3ab326ad715ca8e521d2692b4b35ba6
GinaDeBeukelaer <gientje86>
2016-03-17 18:40:02 +0100

    fix UserIdSource

--- src/main/java/be/ordina/ordineo/config/SocialConfig.java
+++ src/main/java/be/ordina/ordineo/config/SocialConfig.java
@@ -3,6 +3,7 @@ package be.ordina.ordineo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.social.UserIdSource;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
+import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -17,16 +18,23 @@ public class SocialConfig extends SocialConfigurerAdapter {
 
     @Override
     public UserIdSource getUserIdSource() {
-        return new RequestUsernameSource();
+        return new SessionUsernameSource();
     }
 
-    private static final class RequestUsernameSource implements UserIdSource {
+    private static final class SessionUsernameSource implements UserIdSource {
         @Override
         public String getUserId() {
             RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
             HttpServletRequest request = ((ServletRequestAttributes)requestAttributes).getRequest();
 
-            return request.getParameter("username");
+            String userId = (String)request.getSession().getAttribute("username");
+
+            if (StringUtils.isEmpty(userId)) {
+                userId = request.getParameter("username");
+                request.getSession().setAttribute("username", userId);
+            }
+
+            return userId;
         }
     }
 }
