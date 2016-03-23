0324c83340b2605deee7be193537488794f4bf2a
GinaDeBeukelaer <gientje86>
2016-03-23 15:54:48 +0100

    add extra filter tests

--- src/test/java/be/ordina/ordineo/filter/LinkedInFilterTest.java
+++ src/test/java/be/ordina/ordineo/filter/LinkedInFilterTest.java
@@ -1,23 +1,22 @@
 package be.ordina.ordineo.filter;
 
-import be.ordina.ordineo.filter.LinkedInConnectionFilter;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
-import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.social.NotAuthorizedException;
 import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionKey;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.linkedin.api.LinkedIn;
 import org.springframework.web.util.NestedServletException;
-import sun.awt.image.ImageWatched;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 
@@ -35,31 +34,28 @@ public class LinkedInFilterTest {
     @Mock
     ConnectionRepository repository;
 
-
-
     @InjectMocks
     LinkedInConnectionFilter filter;
 
     MockHttpServletResponse response ;
     MockHttpServletRequest request;
-    MockFilterChain chain;
+    FilterChain chain;
     Connection<LinkedIn> connection;
+
     @Before
     public void setup() {
-
         initMocks(this);
+
         response = new MockHttpServletResponse();
         request = new MockHttpServletRequest();
-        chain = new MockFilterChain();
+        chain = mock(FilterChain.class);
         connection = mock(Connection.class);
     }
 
-
-
-
     @Test
-    public void authorizationNeededResponseTest() throws IOException {
+    public void authorizationNeededResponse() throws IOException {
         filter.authorizationNeeded(response);
+
         assertEquals(HttpServletResponse.SC_UNAUTHORIZED, response.getStatus());
         assertEquals("application/json",response.getContentType());
 
@@ -67,30 +63,29 @@ public class LinkedInFilterTest {
         ObjectNode node = factory.objectNode();
         node.put("message", "No connection - authorization with linkedIn needed");
         assertEquals(node.toString(),response.getContentAsString());
-
-
     }
 
     @Test
-    public void testNoPrimaryConnection() throws IOException, ServletException {
-
+    public void noPrimaryConnection() throws IOException, ServletException {
         LinkedIn linkedin = mock(LinkedIn.class);
+
         filter.doFilter(request,response,chain);
+
         assertEquals(HttpServletResponse.SC_UNAUTHORIZED, response.getStatus());
 
         verify(repository).findPrimaryConnection(LinkedIn.class);
         verifyZeroInteractions(repository);
         verifyZeroInteractions(connection);
-
     }
 
 
     @Test
-    public void testWhenConnectionIsExpired() throws IOException, ServletException {
-
+    public void connectionIsExpired() throws IOException, ServletException {
         when(repository.findPrimaryConnection(LinkedIn.class)).thenReturn(connection);
         when (connection.hasExpired()).thenReturn(true);
+
         filter.doFilter(request,response,chain);
+
         assertEquals(HttpStatus.OK.value(), response.getStatus());
 
         verify(repository).findPrimaryConnection(LinkedIn.class);
@@ -100,13 +95,30 @@ public class LinkedInFilterTest {
         verifyZeroInteractions(connection);
     }
 
+    @Test(expected = NestedServletException.class)
+    public void unexpectedException() throws IOException, ServletException {
+        when(repository.findPrimaryConnection(LinkedIn.class)).thenReturn(connection);
+        when(connection.hasExpired()).thenReturn(false);
+
+        doThrow(NestedServletException.class).when(chain).doFilter(request, response);
+
+        filter.doFilter(request, response, chain);
+    }
+
     @Test
-    public void unAuthorized() throws IOException, ServletException {
+    public void unauthorized() throws IOException, ServletException {
         when(repository.findPrimaryConnection(LinkedIn.class)).thenReturn(connection);
         when (connection.hasExpired()).thenReturn(false);
-        //when (chain.doFilter(request,response)).thenThrow(new NestedServletException("test"));
+        doThrow(new NestedServletException("test", new NotAuthorizedException("linkedIn", "not authorized"))).when(chain).doFilter(request, response);
 
+        filter.doFilter(request, response, chain);
 
+        verify(repository).findPrimaryConnection(LinkedIn.class);
+        verify(connection).hasExpired();
+        verify(connection).getKey();
+        verify(repository).removeConnection(any(ConnectionKey.class));
+        verifyZeroInteractions(repository);
+        verifyZeroInteractions(connection);
     }
 
 }
