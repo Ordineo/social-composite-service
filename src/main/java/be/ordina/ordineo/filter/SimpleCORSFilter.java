8b245c73e78353262f353cf3e2877df42bd130c6
GinaDeBeukelaer <gientje86>
2016-03-24 15:36:52 +0100

    update CORS filter

--- src/main/java/be/ordina/ordineo/filter/SimpleCORSFilter.java
+++ src/main/java/be/ordina/ordineo/filter/SimpleCORSFilter.java
@@ -4,8 +4,11 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class SimpleCORSFilter implements Filter {
+
+    private static final String[] allowOrigins = new String[]{"http://localhost:8080", "https://frontend-ordineo.cfapps.io"};
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -17,12 +20,16 @@ public class SimpleCORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        response.addHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-        response.addHeader("Access-Control-Allow-Origin", "https://frontend-ordineo.cfapps.io");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH, HEAD, TRACE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        response.addHeader("Access-Control-Expose-Headers", "location");
+        String originHeader = request.getHeader("Origin");
+
+        if (Arrays.asList(allowOrigins).contains( originHeader )) {
+            response.addHeader("Access-Control-Allow-Origin", originHeader);
+            response.addHeader("Access-Control-Allow-Origin", "https://frontend-ordineo.cfapps.io");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH, HEAD, TRACE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            response.addHeader("Access-Control-Expose-Headers", "location");
+        }
 
         chain.doFilter(request, response);
     }
