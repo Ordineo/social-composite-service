5b52f43e4f9ee7ce949fe31fc0e2c408099c52f5
GinaDeBeukelaer <gientje86>
2016-03-24 15:12:38 +0100

    add empty testcase

---
+++ src/test/java/be/ordina/ordineo/SocialCompositeServiceApplicationTest.java
@@ -0,0 +1,50 @@
+package be.ordina.ordineo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gide on 24/03/16.
+ */
+public class SocialCompositeServiceApplicationTest {
+
+    SocialCompositeServiceApplication application;
+
+    @Before
+    public void setup() {
+        application = new SocialCompositeServiceApplication();
+    }
+
+    @Test
+    public void testLinkedInAuthorizationFilterRegistration() throws Exception {
+
+    }
+
+    @Test
+    public void testLinkedInConnectionFilterRegistration() throws Exception {
+
+    }
+
+    @Test
+    public void testSimpleCORSFilterRegistration() throws Exception {
+
+    }
+
+    @Test
+    public void testLinkedInAuthorizationFilter() throws Exception {
+
+    }
+
+    @Test
+    public void testLinkedInConnectionFilter() throws Exception {
+
+    }
+
+    @Test
+    public void testSimpleCORSFilter() throws Exception {
+
+    }
+
+}
