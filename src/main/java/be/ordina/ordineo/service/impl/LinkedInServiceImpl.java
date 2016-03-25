373e04bea194af095c1fdd7ab3a33e65ab7d9423
GinaDeBeukelaer <gientje86>
2016-03-25 17:14:59 +0100

    fetch original profile picture

--- src/main/java/be/ordina/ordineo/service/impl/LinkedInServiceImpl.java
+++ src/main/java/be/ordina/ordineo/service/impl/LinkedInServiceImpl.java
@@ -3,10 +3,17 @@ package be.ordina.ordineo.service.impl;
 import be.ordina.ordineo.client.EmployeeClient;
 import be.ordina.ordineo.resource.EmployeeResource;
 import be.ordina.ordineo.service.LinkedInService;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.linkedin.api.LinkedIn;
-import org.springframework.social.linkedin.api.LinkedInProfile;
+import org.springframework.social.linkedin.api.LinkedInProfileFull;
+import org.springframework.social.support.URIBuilder;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 /**
  * Created by Hans on 17/03/16.
@@ -14,16 +21,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class LinkedInServiceImpl implements LinkedInService {
 
+    @SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired
     private EmployeeClient employeeClient;
 
     @Override
     public EmployeeResource applyLinkedInDataToEmployee(String username, LinkedIn linkedIn) {
-        LinkedInProfile profile = linkedIn.profileOperations().getUserProfileFull();
+        LinkedInProfileFull profile = linkedIn.profileOperations().getUserProfileFull();
         EmployeeResource employee = employeeClient.getEmployee(username);
         employee.setFirstName(profile.getFirstName());
         employee.setLastName(profile.getLastName());
-        employee.setProfilePicture(profile.getProfilePictureUrl());
+        try {
+            employee.setProfilePicture(GetUserPhotoUrl(linkedIn));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         employee.setDescription(profile.getSummary());
         employee.setLinkedin(profile.getPublicProfileUrl());
         employee.setFunction(profile.getHeadline());
@@ -32,4 +44,14 @@ public class LinkedInServiceImpl implements LinkedInService {
 
         return employee;
     }
+
+    public String GetUserPhotoUrl(LinkedIn linkedIn) throws IOException {
+        String json = linkedIn.restOperations().getForObject(URIBuilder.fromUri("https://api.linkedin.com/v1/people/~/picture-urls::(original)").build(), String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree( json );
+
+        return jsonNode.get("values").get(0).asText();
+    }
+
 }
